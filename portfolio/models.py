27c50220384bd22ab677478b8e1b097d0f109afc
rashi <rashipardeshi870@gmail.com>
2026-05-04 23:36:16 +0530

    Initial portfolio setup: Added Django config and home template

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,29 +1,26 @@
 from django.db import models
 
-# Create your models here.
-
-class Project(models.model):
-    title=models.CharField(max_length=200)
-    description=models.TextField()
-    technology=models.CharField(max_length=200)
-    github_link=models.URLField(blank=True)
-    live_link=models.URLField(blank=True)
-    image=models.ImageField(upload_to='projects/')
-    created_date=models.DateField(auto_now_add=True)
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    technology = models.CharField(max_length=200)
+    github_link = models.URLField(blank=True)
+    live_link = models.URLField(blank=True)
+    image = models.ImageField(upload_to='projects/')
+    created_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title
 
-class Skills(models.model):
-    name=models.CharField(max_length=100)
-    percentage=models.IntegerField(help_text="Skill proficiency % ")
-    category=models.CharField(max_length=50, choice=[
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    percentage = models.IntegerField(help_text="Skill proficiency %")
+    category = models.CharField(max_length=50, choices=[
         ('backend', 'Backend'),
-        ('frontend','Frontend'),
-        ('database','Database'),
-        ('ml','Machine Learning'),
-
+        ('frontend', 'Frontend'),
+        ('database', 'Database'),
+        ('ml', 'Machine Learning'),
     ])
-    
+
     def __str__(self):
         return self.name
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from .models import Project, Skill
 
-# Create your views here.
+def home(request):
+    projects = Project.objects.all().order_by('-created_date')
+    skills = Skill.objects.all()
+    
+    # Your personal data 
+    context = {
+        'name': 'Pardeshi Rashi Ramnarayan',  
+        'title': 'Python Full Stack Developer',
+        'bio': 'BCS Graduate 2026 | Python | Django | SQL Enthusiast',
+        'email': 'rashipardeshi870@gmail.com',
+        'phone': '+91 8767699147',
+        'location': 'Pune, India',
+        'github': 'https://github.com/raship8605',
+        'linkedin': 'https://www.linkedin.com/in/pardeshi-rashi8605/',
+        'projects': projects,
+        'skills': skills,
+        'education': [
+            {'degree': 'BCS (Computer Science)', 
+             'college': 'MGMs college of CS and IT, Nanded', 
+             'year': '2023-2026',
+             'percentage': '85%'},
+            {'degree': 'HSC', 
+             'college': 'Shree shivaji junoir college', 
+             'year': '2021-2023',
+             'percentage': '69%'},
+        ],
+        'certifications': [
+            'Python Full Stack - Kiran Academy, Pune',
+            
+        ]
+    }
+    return render(request, 'portfolio/home.html', context)
